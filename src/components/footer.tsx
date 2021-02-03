9c8890f86012fd3ef7b55da31dcc146b40bae07e
asimhyff <asimhyff@users.noreply.github.com>
2021-02-03 22:30:31 +0500

    updated

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -2,7 +2,7 @@ export const Footer: React.FC = () => {
 	return (
 		<footer className='footer'>
 			<a href='https://github.com/asimhyff' target='blank'>
-				built & designed by Asim Hafeez.
+				built & designed by Qasim Hafeez.
 			</a>
 		</footer>
 	)
